1df5464cd23bb210367061a1840daaa9a489b9f2
s2yuuki2s <116290501+s2yuuki2s@users.noreply.github.com>
2025-12-17 10:15:48 +0700

    Change train size

--- include/constants.h
+++ include/constants.h
@@ -6,7 +6,7 @@ const int IMAGE_HEIGHT = 32;
 const int IMAGE_DEPTH = 3;
 const int IMAGE_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT * IMAGE_DEPTH;
 
-const int NUM_TRAIN_SAMPLES = 20000;
+const int NUM_TRAIN_SAMPLES = 10000;
 const int NUM_TEST_SAMPLES = 10000;
 const int NUM_BATCHES = 5;
 
